42b48ec328922b3765051d4ae3c46b8b6191b4f6
ytj <672574183@qq.com>
2017-12-25 19:53:10 +0800

    have solved the speed question:because I should closed the plot picture. the code wait for closing the picture.

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,7 +31,11 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 from sklearn.neighbors import KNeighborsClassifier
-clf = KNeighborsClassifier(n_neighbors=15)
+clf = KNeighborsClassifier(n_neighbors=8)    
+# n_neighbors:(5,0.92)(6,7,9,0.936)(8,"0.944")(10,12,0.932)(15,0.928)
+
+"""from sklearn.ensemble import AdaBoostClassifier  
+clf = AdaBoostClassifier()    # 0.924   """
 clf.fit(features_train, labels_train) 
 pred = clf.predict(features_test)
 
