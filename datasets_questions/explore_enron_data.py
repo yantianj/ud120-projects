908c1886aff2f11b67824df767f51ce6b58b0041
ytj <672574183@qq.com>
2017-12-26 14:44:39 +0800

    udacity homework:lesson 6:18~20

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,27 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-print(len(enron_data))
+""" enron_data :Two-dimensional dictionary 
+enron_data_1d is name of each person,enron_data_2d is features of each person"""
+
+### length of enron_data_1d and name of each person
+print(len(enron_data))    
+print(enron_data.keys())    
+
+### length of enron_data_2d and features of each person
+print(len(enron_data["METTS MARK"])) 
+print(enron_data["METTS MARK"].keys())
+
+### "POI"(Person of interest):the number of POI==1 in enron_data.
+person_poi = 0
+person_name = enron_data.keys()
+for num in range(len(person_name)):
+	if enron_data[person_name[num]]['poi'] == 1 :
+		person_poi = person_poi+1
+print (person_poi)
+		
+###practice: 
+print("the total value of the stock belonging to James Prentice is %d" % (enron_data["PRENTICE JAMES"]["total_stock_value"]))
+print("the number of emails from_this_person_to_poi is %d" % (enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]))
+print("the value of exercised_stock_options belonging to Jeffrey Skilling is %d" % (enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]))
+
