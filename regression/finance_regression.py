a86e9198347fd1103828e3b2a2de993a5e9a93f3
ytj <672574183@qq.com>
2017-12-27 21:23:47 +0800

    udacity homework: lesson7: 41~47

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -21,7 +21,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+features_list = ["bonus", "salary"]     # long_term_incentive
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,13 +37,17 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
-
-
-
-
-
-
-
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit (feature_train, target_train)
+slope = reg.coef_
+print "slope :",slope
+intercept = reg.intercept_
+print "intercept :",intercept
+training_score = reg.score(feature_train, target_train)
+testing_score = reg.score(feature_test, target_test)
+print "training score :", training_score
+print "testing score :", testing_score
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -58,12 +62,16 @@ plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
 
 
-
+ 
 ### draw the regression line, once it's coded
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+new_slope = reg.coef_
+print "new slope :",new_slope
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
