97ec6619060a467686fcdf34e97d0a60a5bc0ba6
ytj <672574183@qq.com>
2017-12-28 09:20:53 +0800

    udacity ohmework: lesson8: 10,11

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,13 +25,13 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
-
-
-
-
-
-
-
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit (ages_train, net_worths_train)
+slope = reg.coef_
+print "slope :",slope
+testing_score = reg.score(ages_test, net_worths_test)
+print "testing score :", testing_score
 
 
 
