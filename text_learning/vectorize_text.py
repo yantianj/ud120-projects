b60c0aee0a61362413a2b18afb2a4f13f78afe51
ytj <672574183@qq.com>
2018-01-03 15:40:46 +0800

    udacity: lesson11: 19

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -44,25 +44,27 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            # print path
             email = open(path, "r")
-
+	    # print email
             ### use parseOutText to extract the text from the opened email
-
+	    word = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            word = word.replace("sara ","").replace("shackleton ","").replace("chris ","").replace("germani ","")
             ### append the text to word_data
-
+	    word_data.append(word)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
+            if name == "sara":
+		from_data.append(0)
+	    elif name == "chris":
+		from_data.append(1)
             email.close()
 
 print "emails processed"
 from_sara.close()
 from_chris.close()
-
+# print word_data[152]
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
