bd865a430b704b6f6d576fd6657ab482a97d4603
ytj <672574183@qq.com>
2018-01-08 11:16:20 +0800

    udacity: lesson14: cross validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -24,9 +24,14 @@ features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features,labels, test_size=0.3, random_state=42)
 
-
-
-### it's all yours from here forward!  
-
-
+### it's all yours from here forward!
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print accuracy
